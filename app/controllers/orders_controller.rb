b2dfccd6b902682b5abd504dd4054d0789c83f6b
avocado-don <hatonomuneniku79@yahoo.co.jp>
2023-03-09 14:28:19 +0900

    タスク#10-商品購入機能
    商品をクレジットカード決済で購入する機能を実装から単体テストまで行う。

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,38 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @item = Item.find(params[:item_id])
+    if current_user.id == @item.user_id || @item.order
+      redirect_to root_path
+    end
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :street, :building, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: Item.find(params[:item_id]).price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
+  end
+end
